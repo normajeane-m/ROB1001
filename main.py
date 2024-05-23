ba0ffd8d76e9ed22b0e91e3e69dcf3ec66132d83
normajeane-m <40226545+normajeane-m@users.noreply.github.com>
2024-05-23 16:47:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import streamlit as st
 
 st.title('기초로봇공학개론 수업(2024.05.23)')
 
-# Using the "with" syntax
-with st.form(key='my_form'):
-	text_input = st.text_input(label='조 번호')
-	genre = st.radio("성공 유무",
-	    [":rainbow[성공]", "***진행중***", "문제 발생 :movie_camera:"],
-	    captions = ["뿌듯", "기다려주세요", "도와주세요"])
+# # Using the "with" syntax
+# with st.form(key='my_form'):
+# 	text_input = st.text_input(label='조 번호')
+# 	genre = st.radio("성공 유무",
+# 	    [":rainbow[성공]", "***진행중***", "문제 발생 :movie_camera:"],
+# 	    captions = ["뿌듯", "기다려주세요", "도와주세요"])
 	
-	submit_button = st.form_submit_button(label='Submit')
+# 	submit_button = st.form_submit_button(label='Submit')
+
+# 	if submit_button:
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
 	
