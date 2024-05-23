a7c1b5692dd69ebf0686a3172f909374b2a165df
normajeane-m <40226545+normajeane-m@users.noreply.github.com>
2024-05-23 16:03:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title('기초로봇공학개론 수업(2024.05.23( :sunglasses:')
+st.title('기초로봇공학개론 수업(2024.05.23)')
+
+# Using the "with" syntax
+with st.form(key='my_form'):
+	text_input = st.text_input(label='Enter some text')
+	submit_button = st.form_submit_button(label='Submit')
