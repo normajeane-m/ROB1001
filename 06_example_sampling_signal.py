d5a8f7d8008ef19c4926094c17f6e7b2a5c217f7
shs-vision <hyunsoo.shin@outlook.com>
2025-04-09 22:53:01 +0900

    init

---
+++ 06_example_sampling_signal.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Analog signal
+f = 2   # Freq. (5Hz)
+T = 1 / f
+t_analog = np.linspace(0, 2 * T, 1000)  # Time vector
+analog_signal = np.sin(2 * np.pi * f * t_analog)  # Signal
+
+# Sampling
+sampling_rate = 5
+t_sampled = np.arange(0, 2 * T, 1/sampling_rate)  # Sampled time vector 
+sampled_signal = np.sin(2 * np.pi * f * t_sampled)  # Sampled signal
+
+# Visual
+plt.figure(figsize=(10, 6))
+plt.plot(t_analog, analog_signal, label="Analog Signal", linewidth=2)
+#plt.stem(t_sampled, sampled_signal, 'r', markerfmt='ro', basefmt=" ", linefmt="r-", label="Sampled Signal")
+plt.title("Analog Signal and Sampled Signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ README.md
@@ -0,0 +1,18 @@
+# Course Information  
+
+- **Course Code:** ROB1001 
+- **Course Title:** Basic Introduction to Robotics
+- **Semester:** 2025-1
+- **Instructor:** [Hyunsoo Shin](https://shs-vision.github.io)
+- **Teaching Assistants:**  
+  - Seongyeop Lee (ehfdlclsrn22@hanyang.ac.kr)  
+  - Dohyeon Kim (kdh9904@hanyang.ac.kr)  
+
+---
+
+## Announcements  
+
+This repository includes materials for the following weeks:  
+- **Week 6:** Analog signal
+- **Week 7:** TODO
+
